2456545ce58d0d97f5a48c1803e9434d28ccdd56
donjulio.dev <julioejurado@icloud.com>
2023-05-31 18:12:03 -0600

    Similarity API

---
+++ src/app/components/ui/Paragraph.tsx
@@ -0,0 +1,32 @@
+import { FC, HTMLAttributes, forwardRef } from 'react'
+import { VariantProps, cva } from 'class-variance-authority' 
+
+interface ParagraphProps {}
+
+const paragraphVariants = cva("max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center",
+{ 
+    variants: {
+        size: {
+            default: 'text-base sm:text-lg',
+            sm: 'text-sm, sm:text-base'
+        }
+    }, 
+    defaultVariants:{
+        size: 'default', 
+    }
+}
+
+)
+
+interface ParagraphProps extends HTMLAttributes< HTMLParagraphElement >, 
+ VariantProps< typeof paragraphVariants > {
+
+ }
+
+const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>( ({
+    className, size, children, ...props
+}) => {
+    return <div>Paragraph</div>
+  }  ) 
+
+export default Paragraph    
